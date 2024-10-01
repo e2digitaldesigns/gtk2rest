51012b18b03b358a1b5bb5acf7d8e1561e006817
Casana E. Bey <info@expresslayouts.com>
2024-09-30 22:31:41 -0400

    Twurple Logic

--- src/bots/twitch/index copy 2.ts
+++ src/bots/twitch/index copy 2.ts
@@ -6,8 +6,6 @@ const clientId = process.env.TWITCH_CLIENT_ID as string;
 const clientSecret = process.env.TWITCH_CLIENT_SECRET as string;
 
 let twitchBotClient: Bot | undefined;
-let messageListener: any;
-let disconnectListener: any;
 
 const authProvider = new RefreshingAuthProvider({
   clientId,
@@ -18,7 +16,7 @@ authProvider.onRefresh(async (userId, newTokenData) =>
   console.log(`Refreshed token for user ${userId}: ${newTokenData.accessToken}`)
 );
 
-export async function twitchBot(initialStart = true): Promise<Bot> {
+export async function twitchBot() {
   const tokenData = await botFunctions.refreshAccessToken();
 
   if (!tokenData) {
@@ -29,46 +27,39 @@ export async function twitchBot(initialStart = true): Promise<Bot> {
 
   twitchBotClient = new Bot({
     authProvider,
-    channels: await botFunctions.getTwitchChannels()
+    channels: await botFunctions.getTwitchChannels(),
+    commands: [
+      createBotCommand("dice", (params, { reply }) => {
+        const diceRoll = Math.floor(Math.random() * 6) + 1;
+        reply(`You rolled a ${diceRoll}`);
+      })
+    ]
   });
 
-  if (initialStart) {
-    twitchBotClientListener();
-  }
+  twitchBotClient.onConnect(() => console.log("Bot connected to Twitch."));
 
-  return twitchBotClient;
-}
-
-const twitchBotClientListener = async () => {
-  if (!twitchBotClient) {
-    throw new Error("The TwitchBot has not been initialized yet.");
-  }
-
-  if (messageListener) await twitchBotClient.removeListener(messageListener);
-  if (disconnectListener) await twitchBotClient.removeListener(disconnectListener);
-  twitchBotClient.removeListener();
-
-  disconnectListener = twitchBotClient.onDisconnect(async error => {
+  twitchBotClient.onDisconnect(async error => {
     console.error("Bot disconnected", error);
 
     await new Promise(resolve => setTimeout(resolve, 5000));
     console.log("Attempting to reconnect...");
 
     try {
-      twitchBotClient = await twitchBot(false);
+      twitchBotClient = await twitchBot();
       console.log("Reconnected to Twitch.");
     } catch (err) {
       console.error("Failed to reconnect:", err);
     }
   });
 
-  messageListener = twitchBotClient.onMessage(async (arg: MessageEvent) => {
+  twitchBotClient.onMessage(async (arg: MessageEvent) => {
     console.log(arg.userDisplayName, "said:", arg.text);
-    if (arg.text === "!test") {
-      twitchBotClient?.say(arg.broadcasterName, "Testing 123");
-    }
+    const user = await arg.getUser();
+    console.log(user.profilePictureUrl);
   });
-};
+
+  return twitchBotClient;
+}
 
 export const sendTwitchChatMessage = (channel: string, message: string) => {
   if (!twitchBotClient) {
