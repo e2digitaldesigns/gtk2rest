012566d0d06cd444233d3ae3134e267930a4469b
Casana E. Bey <info@expresslayouts.com>
2024-11-10 13:22:19 -0500

    Add active to templates

--- src/models/templates.model.ts
+++ src/models/templates.model.ts
@@ -1,70 +1,72 @@
 import { model, Schema, Types } from "mongoose";
 
 export type TemplateTickerConfig = {
-  hasTitle: boolean;
+	hasTitle: boolean;
 };
 export type TemplateTopicConfig = {
-  allowNesting: boolean;
-  hasImage: boolean;
-  hasVideo: boolean;
+	allowNesting: boolean;
+	hasImage: boolean;
+	hasVideo: boolean;
 };
 
 export interface ILinkArray {
-  name: string;
-  param: string;
+	name: string;
+	param: string;
 }
 
 export interface ITemplateImages {
-  contentBox: ITemplateImagesDefault;
-  logo: ITemplateImagesDefault;
-  sponsors: ITemplateImagesDefault;
-  topic: ITemplateImagesDefault;
+	contentBox: ITemplateImagesDefault;
+	logo: ITemplateImagesDefault;
+	sponsors: ITemplateImagesDefault;
+	topic: ITemplateImagesDefault;
 }
 
 export interface ITemplateImagesDefault {
-  maxAmount: number;
-  width: number;
-  height: number;
+	maxAmount: number;
+	width: number;
+	height: number;
 }
 
 type TemplateTypes = "podcast" | "video";
 
 export interface ITemplate {
-  _id: Types.ObjectId;
-  type: TemplateTypes;
-  name: string;
-  urlKey: string;
-  maxHosts: number;
-  thumbnail: string;
-  iconBgColor: string;
-  tickerConfig: TemplateTickerConfig;
-  topicConfig: TemplateTopicConfig;
-  hasSponsor: boolean;
-  images: ITemplateImages;
-  linkArray: ILinkArray[];
+	_id: Types.ObjectId;
+	active: boolean;
+	hasSponsor: boolean;
+	iconBgColor: string;
+	images: ITemplateImages;
+	linkArray: ILinkArray[];
+	maxHosts: number;
+	name: string;
+	thumbnail: string;
+	tickerConfig: TemplateTickerConfig;
+	topicConfig: TemplateTopicConfig;
+	type: TemplateTypes;
+	urlKey: string;
 }
 
 const TemplateSchema = new Schema<ITemplate>({
-  _id: { type: Schema.Types.ObjectId },
-  type: { type: String, required: true, default: "podcast" },
-  name: { type: String, required: true, default: " " },
-  urlKey: { type: String, required: true, default: " " },
-  thumbnail: { type: String },
-  iconBgColor: { type: String, required: true, default: "black" },
-  maxHosts: { type: Number, required: true, default: 1 },
-  tickerConfig: { type: String, required: true, default: { hasTitle: false } },
-  topicConfig: {
-    type: Object,
-    required: true,
-    default: {
-      allowNesting: true,
-      hasImage: true,
-      hasVideo: true
-    }
-  },
-  hasSponsor: { type: Boolean, required: true, default: false },
-  images: { type: Object, required: true, default: {} },
-  linkArray: { type: [], required: true }
+	_id: { type: Schema.Types.ObjectId },
+	active: { type: Boolean, required: true, default: false },
+	type: { type: String, required: true, default: "podcast" },
+	name: { type: String, required: true, default: " " },
+	urlKey: { type: String, required: true, default: " " },
+	thumbnail: { type: String },
+	iconBgColor: { type: String, required: true, default: "black" },
+	maxHosts: { type: Number, required: true, default: 1 },
+	tickerConfig: { type: String, required: true, default: { hasTitle: false } },
+	topicConfig: {
+		type: Object,
+		required: true,
+		default: {
+			allowNesting: true,
+			hasImage: true,
+			hasVideo: true
+		}
+	},
+	hasSponsor: { type: Boolean, required: true, default: false },
+	images: { type: Object, required: true, default: {} },
+	linkArray: { type: [], required: true }
 });
 
 export const TemplateModel = model("templates", TemplateSchema);
--- src/routes/applicationData/applicationData.ts
+++ src/routes/applicationData/applicationData.ts
@@ -5,33 +5,35 @@ import { TemplateModel } from "../../models";
 const router = express.Router();
 
 router.get("/", async (_, res: Response) => {
-  try {
-    const result = await TemplateModel.find();
+	try {
+		const result = await TemplateModel.find({
+			active: true
+		}).exec();
 
-    const returnData = {
-      resultStatus: {
-        errors: null,
-        responseCode: 200,
-        resultMessage: "Application data loaded successfully",
-        success: true
-      },
-      result: {
-        socials,
-        templates: result
-      }
-    };
+		const returnData = {
+			resultStatus: {
+				errors: null,
+				responseCode: 200,
+				resultMessage: "Application data loaded successfully",
+				success: true
+			},
+			result: {
+				socials,
+				templates: result
+			}
+		};
 
-    res.status(200).json(returnData);
-  } catch (error) {
-    res.status(500).send({
-      resultStatus: {
-        success: false,
-        errors: error,
-        responseCode: 400,
-        resultMessage: "Application data not loaded successfully"
-      }
-    });
-  }
+		res.status(200).json(returnData);
+	} catch (error) {
+		res.status(500).send({
+			resultStatus: {
+				success: false,
+				errors: error,
+				responseCode: 400,
+				resultMessage: "Application data not loaded successfully"
+			}
+		});
+	}
 });
 
 export const applicationData = router;
