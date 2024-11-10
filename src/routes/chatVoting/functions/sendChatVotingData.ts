b30f7133d5aa0d822a8b7538ed90e3edec77e1f8
Casana E. Bey <info@expresslayouts.com>
2024-11-10 16:52:41 -0500

    Change date is chat vote

--- src/routes/chatVoting/functions/sendChatVotingData.ts
+++ src/routes/chatVoting/functions/sendChatVotingData.ts
@@ -4,41 +4,42 @@ import { ChatVotingModel } from "../../../models";
 import { mongoObjectId } from "../../../utils/routeUtils";
 
 export async function sendChatVotingData(userId: string, clientId?: string) {
-  try {
-    const dataLimit = 50;
-    const connectedClients = clientId
-      ? clientState.getClientByResId(clientId)
-      : clientState.getClientsByUserId(userId);
+	try {
+		const dataLimit = 50;
+		const connectedClients = clientId
+			? clientState.getClientByResId(clientId)
+			: clientState.getClientsByUserId(userId);
 
-    const result = await ChatVotingModel.aggregate([
-      {
-        $match: {
-          gtkUserId: mongoObjectId(userId),
-          isDeleted: { $ne: true }
-        }
-      },
-      {
-        $group: {
-          _id: "$chatterUsername",
-          username: { $last: "$chatterUsername" },
-          image: { $last: "$chatterImage" },
-          votes: { $sum: "$votes" }
-        }
-      },
-      { $sort: { votes: -1, date: -1 } },
-      { $limit: dataLimit }
-    ]).exec();
+		const result = await ChatVotingModel.aggregate([
+			{
+				$match: {
+					gtkUserId: mongoObjectId(userId),
+					isDeleted: { $ne: true }
+				}
+			},
+			{
+				$group: {
+					_id: "$chatterUsername",
+					username: { $last: "$chatterUsername" },
+					image: { $last: "$chatterImage" },
+					votes: { $sum: "$votes" },
+					latestVoteDate: { $max: "$date" }
+				}
+			},
+			{ $sort: { votes: -1, latestVoteDate: -1 } },
+			{ $limit: dataLimit }
+		]).exec();
 
-    if (result?.length > 0) {
-      result.forEach((element, index) => {
-        element.rank = index + 1;
-      });
-    }
+		if (result?.length > 0) {
+			result.forEach((element, index) => {
+				element.rank = index + 1;
+			});
+		}
 
-    connectedClients.forEach((client: Client) => {
-      if (client) {
-        client.res.write(`data: ${JSON.stringify(result)}\n\n`);
-      }
-    });
-  } catch (error) {}
+		connectedClients.forEach((client: Client) => {
+			if (client) {
+				client.res.write(`data: ${JSON.stringify(result)}\n\n`);
+			}
+		});
+	} catch (error) {}
 }
