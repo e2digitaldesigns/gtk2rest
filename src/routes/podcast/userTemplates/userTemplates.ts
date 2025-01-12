b0f16720aae9f95e2393f69c9712a5011c34404b
Casana E. Bey <info@expresslayouts.com>
2025-01-12 17:21:35 -0500

    Add templateId for client overlay calls

--- src/routes/podcast/userTemplates/userTemplates.ts
+++ src/routes/podcast/userTemplates/userTemplates.ts
@@ -5,8 +5,18 @@ import { verifyToken } from "../../_middleware";
 const router = express.Router();
 
 router.patch("/", verifyToken, async (req: Request, res: Response) => {
-  const data = await templateFunctions.changeUserTemplate(res.locals.userId, req.body.templateId);
-  res.status(data.resultStatus.responseCode).send(data);
+	const data = await templateFunctions.changeUserTemplate(res.locals.userId, req.body.templateId);
+	res.status(data.resultStatus.responseCode).send(data);
+});
+
+router.get("/client", async (req: Request, res: Response) => {
+	const userId = (req.query.uid as string) || "";
+	const templateId = (req.query.tid as string) || "";
+
+	console.log(userId, templateId);
+
+	const data = await templateFunctions.changeUserTemplate(userId, templateId);
+	res.status(data.resultStatus.responseCode).send(data);
 });
 
 export const userTemplates = router;
--- src/routes/webSocket/webSocket.ts
+++ src/routes/webSocket/webSocket.ts
@@ -7,61 +7,84 @@ import { createHostVote, getTemplateFromUserId } from "utils";
 const router = express.Router();
 
 router.get("/", (_, res: Response) => {
-  res.send("Socket Manual");
+	res.send("Socket Manual");
 });
 
 router.get("/manual/:type", function (req: Request, res: Response) {
-  const { action, nodeSendArray } = socketFunctions.socketParseParams(req.url, req.params.type);
-  const socketIO = getSocketServer();
+	const { action, nodeSendArray } = socketFunctions.socketParseParams(req.url, req.params.type);
+	const socketIO = getSocketServer();
 
-  res.send(nodeSendArray);
-  socketIO.emit(action, nodeSendArray);
+	res.send(nodeSendArray);
+	socketIO.emit(action, nodeSendArray);
 });
 
 router.post("/manual/:type", function (req: Request, res: Response) {
-  const { action, nodeSendArray } = socketFunctions.socketParseParams(
-    req.url,
-    req.params.type,
-    req.body
-  );
+	const { action, nodeSendArray } = socketFunctions.socketParseParams(
+		req.url,
+		req.params.type,
+		req.body
+	);
 
-  const socketIO = getSocketServer();
+	const socketIO = getSocketServer();
 
-  res.json({ action, nodeSendArray });
-  socketIO.emit(action, nodeSendArray);
+	res.json({ action, nodeSendArray });
+	socketIO.emit(action, nodeSendArray);
+});
+
+router.get("/client/overlay-controls", async function (req: Request, res: Response) {
+	try {
+		const action = (req.query.action as string) || "";
+		const data = (req.query.data as object) || {};
+		const socket = (req.query.socket as string) || "";
+		const userId = (req.query.uid as string) || "";
+
+		const nodeSendArray = {
+			tid: await getTemplateFromUserId(userId),
+			uid: userId,
+			action,
+			data
+		};
+
+		res.json({ action, nodeSendArray });
+
+		const socketIO = getSocketServer();
+		socketIO.emit(socket, nodeSendArray);
+	} catch (error) {
+		res.status(500).json({ error: "Internal Server Error" });
+	}
 });
 
 router.post("/overlay-controls", verifyToken, async function (req: Request, res: Response) {
-  const { action, data, socket } = req.body;
-  const socketIO = getSocketServer();
+	const { action, data, socket } = req.body;
+	const socketIO = getSocketServer();
 
-  const nodeSendArray = {
-    tid: await getTemplateFromUserId(res.locals.userId),
-    uid: res.locals.userId,
-    action,
-    data
-  };
+	const nodeSendArray = {
+		tid: await getTemplateFromUserId(res.locals.userId),
+		uid: res.locals.userId,
+		action,
+		data
+	};
 
-  res.json({ action, nodeSendArray });
-  socketIO.emit(socket, nodeSendArray);
+	res.json({ action, nodeSendArray });
+	socketIO.emit(socket, nodeSendArray);
 });
 
 router.post(
-  "/overlay-controls/host-vote",
-  verifyToken,
-  async function (req: Request, res: Response) {
-    const { action, socket } = req.body;
+	"/overlay-controls/host-vote",
+	verifyToken,
+	async function (req: Request, res: Response) {
+		const { action, socket } = req.body;
 
-    console.log({ action, socket });
+		console.log({ action, socket });
 
-    const vote = await createHostVote(res.locals.userId, action, socket);
-    if (!vote) {
-      res.status(400).json({ error: "Invalid action" });
-      return;
-    }
+		const vote = await createHostVote(res.locals.userId, action, socket);
+		if (!vote) {
+			res.status(400).json({ error: "Invalid action" });
+			return;
+		}
 
-    res.json({ success: vote });
-  }
+		res.json({ success: vote });
+	}
 );
 
 export const socket = router;
